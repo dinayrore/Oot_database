9d32cb9eca71292a6e788eb67593603dd10e0032
Kristine <kteich88@gmail.com>
2016-10-03 15:09:24 -0400

    Recreated main search method.

--- sql_search_engine.rb
+++ sql_search_engine.rb
@@ -1,32 +1,45 @@
-require_relative 'zelda_database'
-require_relative 'hero_of_time_inventory'
+require 'pg'
 require_relative 'my_option_parser'
-require_relative 'items'
+
+def disable_notices(conn)
+  conn.exec('SET client_min_messages TO WARNING;')
+end
+
+def create_table(conn)
+  conn.exec(
+    'CREATE TABLE IF NOT EXISTS inventory (' \
+    'id SERIAL PRIMARY KEY,' \
+    'item VARCHAR, type VARCHAR,' \
+    'stock VARCHAR, wielder VARCHAR)'
+  )
+end
+
+def find_inventory(conn)
+  conn.exec('SELECT * FROM inventory')
+end
 
 def main
-  zdb = ZeldaDatabase.new
+  conn = PG.connect(dbname: 'lonk')
 
-  zdb.disable_notices
+  disable_notices(conn)
 
-  zdb.create_table
-
-  inventory = HeroOfTimeInventory.new(zdb)
-
-  items = inventory.find
+  create_table(conn)
 
   options = MyOptionParser.new.parse_options
 
-  # item = Items.new() do not pass things through?
-  #
-  # item.find_item(options, inventory) if options.key? :find
-  #
-  # item.add_item(options) if options.key? :add
-  #
-  # item.display_inventory(inventory) if options.key? :display
-  #
-  # item.edit_item(options) if options.key? :edit
-  #
-  # item.remove_item(options) if options.key? :remove
+  item = HeroOfTimeInventory.new(conn)
+
+  inventory = find_inventory(conn)
+
+  item.find_items(options, inventory) if options.key? :find
+
+  item.add_item(options) if options.key? :add
+
+  item.display_inventory(inventory) if options.key? :display
+
+  item.edit_item(options) if options.key? :edit
+
+  item.remove_item(options) if options.key? :remove
 end
 
 main if __FILE__ == $PROGRAM_NAME
